2fd2429b09cb67a1cd4d0e4dd4f475524ab7ab4e
Zaharadine laouali <124533008+Ibnlaouale@users.noreply.github.com>
2023-10-11 06:36:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,153 +1,116 @@
-let seconde = document.querySelector('#countS');
-let munite = document.querySelector('#countM');
-let date2 = new Date();
-let date = Date.now();
-
-
-let heure = document.querySelector('#countH');
-console.log(heure.textContent);
-
 let boutC20 = document.querySelector('#boutC20');
 let boutC5 = document.querySelector('#boutC5');
-
-
 let divTimer = document.querySelector('#divTimer');
 let comeBack = document.querySelector('#comeBack');
+let date = new Date();
+let interval = null;
 
 
-/*#################### LES FONCTIONS  ##########################################*/
+const ul = document.querySelector('ul'); //recuperation des bouttons
+ul.addEventListener('click', (e) => { //ecouteur d'evènement sur chaque boutton en même temps
+  let cible = e.target.id;
+  if (cible === 'boutC20') {
+    let value = 20;
+    chrono(value);
+    afficheChrono();
+    showGoBack(value);
+  } else if (cible === 'boutC5') {
+    let value = 5 * 60;
+    chrono(value)
+    afficheChrono();
+    showGoBack(value)
+  } else if (cible === 'bout15') {
+    let value = 15 * 60;
+    chrono(value)
+    afficheChrono();
+    showGoBack(value)
 
+  } else if (cible === 'btnPD') {
+    let value = 20 * 60;
+    chrono(value)
+    afficheChrono();
+    showGoBack(value)
 
-function afficheChrono(){
-    divTimer.classList.remove('hidden');
-    comeBack.classList.remove('hidden');
-}
+  } else if (cible === 'boutCD30') {
+    let value = 30 * 60;
+    chrono(value)
+    afficheChrono();
+    showGoBack(value)
 
-function cacheChrono(){
-
-    divTimer.classList.add('hidden');
-    comeBack.classList.add('hidden');
-}
-//----------------------------------
-
-function addSeconds(date, seconds) {
-    date.setSeconds(date.getSeconds() + seconds);
-    return date;
-  };
-//------------------------------------------------------
-  function addMunites(date, mn) {
-    date.setMinutes(date.getMinutes() + mn);
-    return date;
-  };
-
-//----------------- la valeur `time` en munites ---------------------
-  function chrono(time){
-
-    if (time < 0) {
-        alert('la valeur doit être positive');
-    } else {
-       let newDate = new Date();
-       let temps = time * 60
-    
-   let interval = setInterval(() => {
-    //  heure.textContent = Math.floor(temps/(60*60)) +':';
-     munite.textContent = Math.floor(temps / 60) + ':';
-     seconde.textContent = Math.floor(temps % 60);
-     
-    temps = temps <= 0 ? 0 : --temps;
-    console.log(temps);
-    
-    if (temps === 0) {
-        clearInterval(interval);
-        cacheChrono();
-    }
-
-    }, 1000);
-    addMunites(newDate, time);
-    comeBack.innerHTML =`Be Back At   ${newDate.getHours()}  :  ${newDate.getMinutes()}  : ${newDate.getSeconds()}`;
-    }
-     
   }
+})
 
-// ######################################################
-
-/*------------------- boutton 20 secondes  ---------------------*/
-
-boutC20.addEventListener('click', ()=>{
-    
-    let date = new Date();
-    let nextTime = date.getSeconds()+20;
-    let time20 = nextTime - date.getSeconds();
-    console.log(time20);
-    seconde.innerHTML = time20;
-    let interval = setInterval(()=>{
-        seconde.innerHTML--;
-        
-        if(seconde.innerHTML<=0){
-            clearInterval(interval);
-            seconde.textContent = '00';
-            cacheChrono();
-        }
-    //   console.log(seconde.innerHTML);
-    }, 1000);
-    addSeconds(date, nextTime)
-    //date.setSeconds(nextTime);
-    afficheChrono();
-    
-    comeBack.innerHTML =`Be Back At   ${date.getHours()}  :  ${date.getMinutes()}  : ${date.getSeconds()}`;    
-});
-
-
-/*------------------- boutton 5mn  ---------------------*/
-boutC5.addEventListener('click', ()=>{
-    chrono(5);
-    afficheChrono();
-
-});
-
-/*------------------- boutton 15mn  ---------------------*/
-const boutTHE = document.getElementById('bout15');
-boutTHE.addEventListener('click', ()=>{
-
-    chrono(15);
-    afficheChrono();
-
-});
-
-/*------------------- boutton 20mn  ---------------------*/
-
-const btnPD = document.getElementById('btnPD');
-
-btnPD.addEventListener('click', ()=>{
-
-
-    chrono(20);
-    afficheChrono();
-});
-
-/*------------------- boutton 30mn  ---------------------*/
-
-const boutCD30 = document.getElementById('boutCD30');
-boutCD30.addEventListener('click', ()=>{
-
-   chrono(30);
-   afficheChrono();
-    
-});
 
 /*------------------- la valeur `entrée` input  ---------------------*/
 
-const enter = document.querySelector('#enter');
+let enter = document.querySelector('#enter');
 
-// console.log(enter);
 
-enter.addEventListener('keyup', (e)=>{
-  if (e.key == 'Enter'){
-    let time = e.target.valueAsNumber;
-    // time = parseInt(time,10);
-    console.log(typeof(time));
-    chrono(time);
-    afficheChrono();
-  }    
+enter.addEventListener('keyup', (e) => {
 
+  if (e.key == 'Enter') {
+    let t = e.target.valueAsNumber;
+    if (t === NaN) {
+      alert('veuillez renseigner tous les champ');
+      return
+    }else
+    if (t < 0) {
+      alert('la valeur saisie doit être positive');
+      return;
+    } else {
+      t = t * 60;
+      chrono(t);
+      afficheChrono();
+      showGoBack(t)
+    }
+
+  }
 });
+
+
+/*================== LES FONCTIONS  ======================*/
+
+
+function afficheChrono() {
+  divTimer.classList.remove('hidden');
+  comeBack.classList.remove('hidden');
+}
+
+function cacheChrono() {
+
+  divTimer.classList.add('hidden');
+  comeBack.classList.add('hidden');
+}
+// //-----------fonction chrono -----------------------
+
+const chrono = (t) => {
+  if (interval !== null) {
+    clearInterval(interval)
+  }
+  interval = setInterval(() => {
+    timer(t)
+    t--;
+    if (t === 0) {
+      clearInterval(interval);
+      cacheChrono();
+    }
+  }, 1000);
+}
+//----------------- fonction timer qui determine les valeur de munite, seconde et heures ---------------------
+function timer(s) {
+  {
+    let heure = Math.floor(s / 3600),
+      munite = Math.floor(s / 60) % 60,
+      seconde = s % 60;
+
+    document.querySelector('#count').innerHTML = `${heure < 10 ? '0' : ''}${heure}:${munite < 10 ? '0' : ''}${munite}:${seconde < 10 ? '0' : ''}${seconde}`;
+  }
+
+}
+
+function showGoBack(t) {
+  let heure = Math.floor(t / 3600),
+    munite = Math.floor(t / 60) % 60,
+    seconde = t % 60;
+  comeBack.innerHTML = `Be Back At  ${heure < 10 ? '0' : ''}${date.getHours() + heure}:${date.getMinutes() + munite}:${date.getSeconds() + seconde}`;
+}
